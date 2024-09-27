8627504ff5e1751848be37a977d76252b7cc7cff
SpireX <SpireX@outlook.com>
2024-09-27 19:22:20 +0200

    test(js-boot): Adding tasks to boot process

--- packages/boot/src/Boot.test.ts
+++ packages/boot/src/Boot.test.ts
@@ -24,4 +24,60 @@ describe("Boot", () => {
             expect(boot).toBeInstanceOf(Boot);
         });
     });
+
+    describe("Adding tasks to boot process", () => {
+        // GIVEN:
+        //  - One task is created
+        //  - Process is created
+        // WHEN: Add task to process
+        // THEN: The task has been added to the process
+        test("Adding one task to boot process", () => {
+            // Arrange -----------
+            const task = Boot.task(() => {});
+            const boot = new Boot();
+
+            // Act ---------------
+            const bootRef = boot.add(task);
+
+            // Assert ------------
+            expect(bootRef).toBe(boot);
+            expect(boot.tasksCount).toBe(1);
+        });
+
+        // GIVEN
+        //  - Two tasks are created
+        //  - Process is created
+        // WHEN: Add tasks to process
+        // THEN: Both tasks have been added to the process.
+        test("Adding multiple tasks to boot process", () => {
+            // Arrange -----------
+            const task1 = Boot.task(() => {});
+            const task2 = Boot.task(() => {});
+            const boot = new Boot();
+
+            // Act ---------------
+            const bootRef = boot.add(task1).add(task2);
+
+            // Assert ------------
+            expect(bootRef).toBe(boot);
+            expect(boot.tasksCount).toBe(2);
+        });
+
+        // GIVEN:
+        //  - One task is created
+        //  - Process is created
+        // WHEN: Add task to process 2 times
+        // THEN: The task is added to the process only once
+        test("Adding same task twice to boot process", () => {
+            // Arrange -----------
+            const task = Boot.task(() => {});
+            const boot = new Boot().add(task);
+
+            // Act ---------------
+            boot.add(task);
+
+            // Assert ------------
+            expect(boot.tasksCount).toBe(1);
+        });
+    });
 });
--- packages/boot/src/Boot.ts
+++ packages/boot/src/Boot.ts
@@ -14,4 +14,29 @@ export class Boot {
     }
 
     // endregion: STATIC METHODS
+
+    // region: FIELDS
+
+    private readonly _tasks: TBootTask[] = [];
+
+    // endregion: FIELDS
+
+    // region: PROPERTIES
+
+    public get tasksCount(): number {
+        return this._tasks.length;
+    }
+
+    // endregion: PROPERTIES
+
+    // region: PUBLIC METHODS
+
+    public add(task: TBootTask): Boot {
+        if (!this._tasks.includes(task)) {
+            this._tasks.push(task);
+        }
+        return this;
+    }
+
+    // endregion: PUBLIC METHODS
 }
